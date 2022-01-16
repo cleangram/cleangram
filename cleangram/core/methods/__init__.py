204f404f6fa6bcdde1446f3eced545fa22ec0ad6
Arwichok <arwichok@gmail.com>
2022-01-16 18:25:58 +0200

    Append GetWebhookInfo

--- cleangram/core/methods/__init__.py
+++ cleangram/core/methods/__init__.py
@@ -3,11 +3,13 @@ from .get_me import GetMe
 from .get_updates import GetUpdates
 from .send_message import SendMessage
 from .set_webhook import SetWebhook
+from .get_webhook_info import GetWebhookInfo
 
 __all__ = [
     "TelegramMethod",
     "GetMe",
     "GetUpdates",
     "SendMessage",
-    "SetWebhook"
+    "SetWebhook",
+    "GetWebhookInfo"
 ]
