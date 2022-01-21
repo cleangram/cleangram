2c2089128500c62b95f8d6712e3097d391956a99
Arwichok <arwichok@gmail.com>
2022-01-21 16:46:39 +0200

    Add env

---
+++ cleangram/env.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, fields
+import os
+from typing import Optional
+
+
+@dataclass(frozen=True)
+class _Env:
+    TELEGRAM_BOT_TOKEN: Optional[str] = None
+    TELEGRAM_API_ENDPOINT: str = "https://api.telegram.org"
+    WEBHOOK_ENDPOINT: str = ""
+
+
+env = _Env(**{f.name: os.environ[f.name] for f in fields(_Env)
+              if f.name in os.environ})
