eade68f6037d695507208d1dd49d27eaf2c510f7
Arwichok <arwichok@gmail.com>
2022-01-25 23:00:41 +0200

    Add Observer

---
+++ cleangram/application/observers/update.py
@@ -0,0 +1,19 @@
+from typing import Generic, TypeVar, List, Callable, Any
+
+from ...core import Bot
+from cleangram.application.observers.base import Observer
+
+U = TypeVar("U")
+
+
+class UpdateObserver(Observer, Generic[U]):
+    def __init__(self):
+        self._handlers: List[Callable[[U, Any], Any]] = []
+
+    def __call__(self, handler: Callable[[U, Any], Any]):
+        self._handlers.append(handler)
+
+    async def notify(self, event: U, bot: Bot):
+        for handler in self._handlers:
+            if resp := await handler(event, bot):
+                return resp
